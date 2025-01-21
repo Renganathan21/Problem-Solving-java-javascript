27c1bc34d1f64e20ba06261a22f7798cbeae7e0d
Renga Nathan A <114427016+Renganathan21@users.noreply.github.com>
2025-01-21 16:01:08 +0000

    change in problem 1

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 class Main {
     public static void main(String[] args) {
-        int[] arr = {2,3,4,4,5};
+        int[] arr = {2,3,4,4,5,6,5};
          ArrayList<Integer> res = new ArrayList<>();
         for (int i = 0 ; i< arr.length ; i++){
             for(int j = i +1 ; j < arr.length ; j++){
