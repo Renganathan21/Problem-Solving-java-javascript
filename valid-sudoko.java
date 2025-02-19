009222556c8c32afef130be76fc2cfa280907172
Renga Nathan A <114427016+Renganathan21@users.noreply.github.com>
2025-02-19 23:00:09 +0530

    valid sudoko - todays problem

---
+++ valid-sudoko.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for(int i =0; i<9;i++){
+            for(int j = 0 ; j<9; j++){
+                char num = board[i][j];
+                if(num != '.'){
+                    if(!seen.add(num+"row at "+i) || 
+                    !seen.add(num+"col at " + j) ||
+                    !seen.add(num + "box" + i/3 + "-" + j/3)
+                        
+                    ){
+                        return false;
+                    }
+                }
+            }
+
+        }
+        return true;
+    }
+}
