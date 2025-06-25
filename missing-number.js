f29c03c1a52eb21f132cd87bd4a33b8517d179e8
Renga Nathan A <114427016+Renganathan21@users.noreply.github.com>
2025-06-25 21:29:33 +0530

    change in missing number alt

--- missing-number.js
+++ missing-number.js
@@ -10,4 +10,12 @@ for(let i =0;i<a.length - 1;i++){
     }
 }
 
-console.log(m)
+console.log(m);
+
+
+const a1 = [3,1,2,5,0];
+const n = a1.length;
+const s = (n*(n+1))/2;
+const ms = a1.reduce((acc,num) => acc+num,0);
+
+console.log(s-ms);
